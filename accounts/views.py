ce905942053cfa62f4016df65faae8ece00e6044
Harini-04 <harinikurella2003@gmail.com>
2025-12-08 18:56:14 +0530

    Verified authenticated user endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterAPIView(APIView):
     permission_classes=[]
@@ -13,3 +14,17 @@ class RegisterAPIView(APIView):
             serializer.save()
             return Response({"message":"User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MeAPIView(APIView):
+    permission_classes=[IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        data = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
+        return Response(data, status=status.HTTP_200_OK)
